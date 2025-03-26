350c93066ebc2d26c4bb82e8fbbd279797fd21c1
Michael Kuehl <hello@txs.vc>
2025-03-26 17:39:26 +0100

    worked on some podman kinks

--- roles/prepare_exporter/files/exporter_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-source jumpstarter/.venv/bin/activate
-
-jmp admin create exporter outpost-mock -l shadowcar/board mock --save --out ./outpost-mock-exporter.yaml
-
-jmp admin create client outpost-mock --save --unsafe
